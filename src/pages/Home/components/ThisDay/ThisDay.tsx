1eae2eca7e9d1a160e97cf2e751d0fc62cf08d07
Dmytro Svyrydenko <20019dima@gmail.com>
2024-02-19 00:06:35 +0200

    add thisday and thisdayinfo

---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import s from './ThisDay.module.scss'
+import Select from 'react-select';
+
+
+interface Props { }
+
+export const ThisDay = (props: Props) => {
+
+  return (
+    <header className={s.header}>
+    </header>
+  );
+};
+
